c417b5c7d47a2ecff7a145200bc7b85bf1e2d854
severusDude <fajarsaladin05@gmail.com>
2024-03-12 12:05:11 +0700

    fix(admin): fix category plural name in admin view

--- marketplace/item/models.py
+++ marketplace/item/models.py
@@ -5,3 +5,10 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
